a81a49776eda90649abb532a02336a0a97636713
alexander <alexander@debian>
2016-01-03 21:49:41 +0300

    Refactoring frontend API base servlet

--- server/src/main/java/ispb/frontend/servlets/CityServlet.java
+++ server/src/main/java/ispb/frontend/servlets/CityServlet.java
@@ -1,42 +1,31 @@
 package ispb.frontend.servlets;
 
 import ispb.base.db.dataset.CityDataSet;
-import ispb.base.frontend.entity.CityRestEntity;
 import ispb.base.frontend.response.CityListRestResponse;
 import ispb.base.frontend.response.CityRestResponse;
-import ispb.base.frontend.utils.ResponseCodes;
-import ispb.frontend.utils.BaseServlet;
+import ispb.base.frontend.utils.RestResponse;
+import ispb.frontend.utils.ApiBaseServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.Map;
 
-public class CityServlet extends BaseServlet {
+public class CityServlet extends ApiBaseServlet {
 
-    protected void doGet( HttpServletRequest request,
-                          HttpServletResponse response ) throws ServletException, IOException {
-        String[] params = request.getPathInfo().split("/");
-        if (params.length > 1){
-            int id = 0;
-            try {
-                id = Integer.parseInt(params[1]);
-            }
-            catch (Exception e){
-                this.writeFailMessage(response, "The id must be integer.", ResponseCodes.DATA_FORMAT_INCOMPATIBLE);
-                return;
-            }
-            CityDataSet city = getApplication().getDaoFactory().getCityDao().getById(id);
-            if (city == null){
-                this.writeFailMessage(response, "City not found.", ResponseCodes.NOT_FOUND);
-                return;
-            }
-            CityRestResponse resp = new CityRestResponse(city);
-            writeJson(response, resp.toJson());
-        }
-        else {
-            CityListRestResponse resp = new CityListRestResponse(getApplication().getDaoFactory().getCityDao().getAll());
-            writeJson(response, resp.toJson());
-        }
+    protected RestResponse getEntity(long id, Map<String, String[]> params){
+        CityDataSet city = getApplication().getDaoFactory().getCityDao().getById(id);
+        if (city == null)
+            return null;
+        return new CityRestResponse(city);
+    }
+
+    protected RestResponse getEntityList(Map<String, String[]> params){
+        return new CityListRestResponse(getApplication().getDaoFactory().getCityDao().getAll());
+    }
+
+    protected RestResponse delEntity(long id, Map<String, String[]> params){
+        return null;
+    }
+
+    protected RestResponse delEntityList(Map<String, String[]> params){
+        return null;
     }
 }
--- server/src/main/java/ispb/frontend/utils/ApiBaseServlet.java
+++ server/src/main/java/ispb/frontend/utils/ApiBaseServlet.java
@@ -1,33 +1,60 @@
 package ispb.frontend.utils;
 
-import ispb.base.frontend.utils.Verifiable;
+import ispb.base.frontend.utils.ResponseCodes;
+import ispb.base.frontend.utils.RestResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
-public abstract class ApiBaseServlet<T extends Verifiable> extends BaseServlet {
+public abstract class ApiBaseServlet extends BaseServlet {
 
-    protected int minAccessLevel(){
-        return 0;
-    }
+    protected void service( HttpServletRequest request,
+                            HttpServletResponse response ) throws ServletException, IOException {
 
-    protected abstract Class<T> getRestType();
+        Long id = null;
+        String path = request.getPathInfo();
+        if (path != null) {
+            String[] params = request.getPathInfo().split("/");
+            if (params.length > 1)
+                try {
+                    id = Long.parseLong(params[1]);
+                }
+                catch (Exception e){
+                    this.writeFailMessage(response, "The id must be integer.", ResponseCodes.DATA_FORMAT_INCOMPATIBLE);
+                    return;
+                }
+        }
 
-    @Override
-    protected void doGet( HttpServletRequest request,
-                          HttpServletResponse response ) throws ServletException, IOException{
-
-    }
-
-    @Override
-    protected void doPost( HttpServletRequest request,
-                           HttpServletResponse response ) throws ServletException, IOException {
-
-        T reqObj = this.prepareJsonRequest(request, response, getRestType());
-        if (reqObj == null)
+        String method = request.getMethod();
+        RestResponse restResponse;
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        if (Objects.equals(method, "GET") && id == null)
+            restResponse = getEntityList(parameterMap);
+        else if (Objects.equals(method, "GET") && id != null)
+            restResponse = getEntity(id, parameterMap);
+        else if (Objects.equals(method, "DELETE") && id == null)
+            restResponse = delEntity(id, parameterMap);
+        else if (Objects.equals(method, "DELETE") && id != null)
+            restResponse = delEntityList(parameterMap);
+        else {
+            this.writeFailMessage(response, "Method not implemented.", ResponseCodes.METHOD_NOT_ALLOWED);
             return;
+        }
+
+        if (restResponse == null){
+            this.writeFailMessage(response, "Not found.", ResponseCodes.NOT_FOUND);
+            return;
+        }
+
+        writeJson(response, restResponse.toJson());
     }
 
+    protected abstract RestResponse getEntity(long id, Map<String, String[]> params);
+    protected abstract RestResponse getEntityList(Map<String, String[]> params);
+    protected abstract RestResponse delEntity(long id, Map<String, String[]> params);
+    protected abstract RestResponse delEntityList(Map<String, String[]> params);
 }
