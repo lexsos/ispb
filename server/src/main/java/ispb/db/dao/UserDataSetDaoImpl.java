dd706b2f4a80986b1bc5c8bbb332c65156645f2c
Alexander <alexander@avia35.ru>
2016-03-29 16:33:37 +0300

    Fix code style

--- server/src/main/java/ispb/db/dao/UserDataSetDaoImpl.java
+++ server/src/main/java/ispb/db/dao/UserDataSetDaoImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class UserDataSetDaoImpl extends BaseDao implements UserDataSetDao {
 
-    private AppResources resources;
+    private final AppResources resources;
 
     public UserDataSetDaoImpl(SessionFactory sessions, AppResources resources){
         super(sessions);
