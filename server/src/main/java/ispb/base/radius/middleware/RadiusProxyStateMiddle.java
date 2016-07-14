7ffe6ddf49480c5a8ed94daae691ffba413922a8
Alexander <alexander@avia35.ru>
2016-07-14 10:05:22 +0300

    Add RADIUS middleware for copy Proxy-State attribute

---
+++ server/src/main/java/ispb/base/radius/middleware/RadiusProxyStateMiddle.java
@@ -0,0 +1,17 @@
+package ispb.base.radius.middleware;
+
+
+import ispb.base.radius.attribute.RadiusAttribute;
+import ispb.base.radius.exception.RadiusException;
+import ispb.base.radius.packet.RadiusPacket;
+
+public class RadiusProxyStateMiddle implements RadiusMiddleOut {
+
+    public static int TYPE_PROXY_STATE = 33;
+
+    public void out(RadiusPacket request, RadiusPacket reply, byte[] secret) throws RadiusException {
+        for (RadiusAttribute attribute: request.getAttributeList())
+            if (attribute.getType() == TYPE_PROXY_STATE)
+                reply.addAttribute(attribute);
+    }
+}
